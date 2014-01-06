9e72c5d8c33e259fe7e83c22dfc9f16b8915f133
Vitaliy Vokhmin <vitaliy.vokhmin@gmail.com>
2014-01-06 15:40:56 +0200

    Added calendar sync. Copy-paste for now.

--- CalCardDavSync/ThisAddIn.cs
+++ CalCardDavSync/ThisAddIn.cs
@@ -72,11 +72,14 @@ namespace CalCardDavSync
                     break;
                 }
             }
+            // TODO fix folder search.
             olFolderContacts = store.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderContacts) as Outlook.Folder;
             olFolderCalendar = store.GetDefaultFolder(Outlook.OlDefaultFolders.olFolderCalendar) as Outlook.Folder;
 
             olFolderContacts.UserDefinedProperties.Add("remoteID", Outlook.OlUserPropertyType.olText);
             olFolderContacts.UserDefinedProperties.Add("modifyDate", Outlook.OlUserPropertyType.olDateTime);
+            olFolderCalendar.UserDefinedProperties.Add("remoteID", Outlook.OlUserPropertyType.olText);
+            olFolderCalendar.UserDefinedProperties.Add("modifyDate", Outlook.OlUserPropertyType.olDateTime);
 
             tmpFilnameContacts = Path.GetTempFileName().Replace(".tmp", ".vcf");
             tmpFilnameCalendar = Path.GetTempFileName().Replace(".tmp", ".ics");
@@ -85,7 +88,7 @@ namespace CalCardDavSync
             session.Credentials = new NetworkCredential(login, password);
         }
 
-        private void IterateItems<resourceType>(IFolder folder, Outlook.Folder olFolder, string tmpFilename)
+        private void IterateContactsItems(IFolder folder, Outlook.Folder olFolder, string tmpFilename)
         {
             List<string> processedIds = new List<string>();
             IHierarchyItem[] remoteItems = folder.GetChildren();
@@ -95,7 +98,7 @@ namespace CalCardDavSync
                 processedIds.Add(remoteItem.DisplayName);
                 IResource resource = folder.GetResource(remoteItem.DisplayName);
 
-                Outlook.ContactItem existItem = (Outlook.ContactItem) olFolder.Items.Find(String.Format("[remoteID] = '{0}'", remoteItem.DisplayName));
+                Outlook.ContactItem existItem = (Outlook.ContactItem)olFolder.Items.Find(String.Format("[remoteID] = '{0}'", remoteItem.DisplayName));
                 if (existItem != null)
                 {
                     // if contact is not modified then skip.
@@ -145,11 +148,80 @@ namespace CalCardDavSync
             }
         }
 
+        private void IterateCalendarItems(IFolder folder, Outlook.Folder olFolder, string tmpFilename)
+        {
+            List<string> processedIds = new List<string>();
+            IHierarchyItem[] remoteItems = folder.GetChildren();
+            foreach (IHierarchyItem remoteItem in remoteItems)
+            {
+                if (remoteItem.ItemType != ItemType.Resource) continue;
+                processedIds.Add(remoteItem.DisplayName);
+                IResource resource = folder.GetResource(remoteItem.DisplayName);
+
+                Outlook.AppointmentItem existItem = (Outlook.AppointmentItem)olFolder.Items.Find(String.Format("[remoteID] = '{0}'", remoteItem.DisplayName));
+                if (existItem != null)
+                {
+                    // if contact is not modified then skip.
+                    // TODO fix dates. Time is rounded in UserProperty. WTF?
+                    if (remoteItem.LastModified != existItem.UserProperties.Find("modifyDate").Value)
+                    {
+                        existItem.Delete();
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+
+                // TODO replace this code with in-memory procedure
+                using (Stream stream = resource.GetReadStream())
+                {
+                    using (Stream f = File.OpenWrite(tmpFilename))
+                    {
+                        CopyStream(stream, f);
+                    }
+                }
+
+                // TODO fix bug with encodings. UTF8 is recognized as CP1252. WTF?
+                Outlook.AppointmentItem item = (Outlook.AppointmentItem)Application.Session.OpenSharedItem(tmpFilename);
+                File.Delete(tmpFilename);
+
+                item.UserProperties.Add("remoteID", Outlook.OlUserPropertyType.olText).Value = remoteItem.DisplayName;
+                item.UserProperties.Add("modifyDate", Outlook.OlUserPropertyType.olDateTime).Value = remoteItem.LastModified;
+                item.Move(olFolder);
+                item.Save();
+            }
+            string filter = String.Empty;
+            foreach (string id in processedIds)
+            {
+                filter += String.Format("[remoteID] <> '{0}' And ", id);
+            }
+            Outlook.Items itemsForDelete = olFolder.Items;
+            if (filter.Length > 0)
+            {
+                filter = filter.Substring(0, filter.Length - 5);
+                itemsForDelete = itemsForDelete.Restrict(filter);
+            }
+            foreach (Outlook.AppointmentItem item in itemsForDelete)
+            {
+                item.Delete();
+            }
+        }
+
         private void SyncContacts()
         {
             while (threadExecuteSyncContacts)
             {
-                IterateItems<Outlook.ContactItem>(session.OpenFolder(urlContacts), olFolderContacts, tmpFilnameContacts);
+                IterateContactsItems(session.OpenFolder(urlContacts), olFolderContacts, tmpFilnameContacts);
+                Thread.Sleep(threadSleepTime);
+            }
+        }
+
+        private void SyncCalendar()
+        {
+            while (threadExecuteSyncCalendar)
+            {
+                IterateCalendarItems(session.OpenFolder(urlCalendar), olFolderCalendar, tmpFilnameCalendar);
                 Thread.Sleep(threadSleepTime);
             }
         }
